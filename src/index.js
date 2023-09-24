613db8c9b0dec78c830fd662bdb62fd10d589e6e
EdMorales2986 <ed.morales6892@gmail.com>
2023-09-24 18:49:37 -0400

    Modif 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import express from "express";
-import router from "./routes/index.routes.js"
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(router)
+/*
+    EJS: it let us embed JavaScript code in a template language that is then used to generate HTML.
+    this module has a good integration with ExpressJS so we don't really need to import through 'require'.
+*/
+// Settings
+app.set("views", path.join(__dirname, "static/views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "static/public")));
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(4000)
-console.log(`http://localhost:4000`)
+// Router
+app.use(require("./routes/index.routes"));
+
+app.listen(4000);
+console.log(`http://localhost:4000`);
