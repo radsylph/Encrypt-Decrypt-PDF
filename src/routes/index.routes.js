01517b321f90682332785026a710ee23de198655
radsylph <andresa@devtop.io>
2023-09-27 03:22:11 -0400

    feat: trying to add more routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,15 @@
 const { Router } = require("express");
-const { hello } = require("../controllers/index.controllers.js");
+const {
+  hello,
+  upload,
+  multerUpload,
+} = require("../controllers/index.controllers.js");
 
 const router = Router();
+
+//ANCHOR - Multer Middleware Config
+
 router.get("/", hello);
+router.post("/upload", multerUpload, upload);
 
 module.exports = router;
